2135c068cb2e34f1126debbb9d1d10ebc5550924
Elliot Glasenk <eglasenk@box.com>
2019-02-23 13:46:13 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {data} from './static-data';
+import Sidebar from './Sidebar';
+import Chat from './Chat';
+import moment from 'moment';
 
 class App extends Component {
+
+  state = {
+    user: 'Dave',
+    activeChat: 'Dave',
+    messages: data.messages
+  }
+
+  //helper function
+  changeActiveChat = chat => {
+    this.setState({
+      activeChat: chat,
+    })
+  }
+
+  sendNewMessage = (message, activeChat) => {
+    let copy = {...this.state.messages};
+    copy[activeChat] = typeof copy[activeChat] === 'undefined' ? copy[activeChat] = [message] : [...copy[activeChat], message];
+    this.setState({
+      messages: copy
+    })
+  }
+
   render() {
+    const user = this.state.user;
+    const activeChat = this.state.activeChat;
+    const allChatMessages = this.state.messages;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Sidebar 
+          channels={data.channels} 
+          people={data.people} 
+          activeChat={activeChat} 
+          onChatChange={this.changeActiveChat} />
+        <Chat 
+          user={user}
+          activeChat={activeChat} 
+          allChatMessages={allChatMessages}
+          onSendMessage={this.sendNewMessage}
+        />
       </div>
     );
   }
